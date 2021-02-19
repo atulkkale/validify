2315e27c76822282813d4ef5227ac9bfa7691b04
Atul Kale <kaleatul9@gmail.com>
2021-02-19 15:04:53 +0530

    js code implemented.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,64 @@
-/* Author: 
+var form = document.getElementById('form');
+var firstnamev = document.getElementById('first_name');
+var lastnamev = document.getElementById('last_name');
+var statev = document.getElementById('state');
+var yourmassagev = document.getElementById('your_massage');
+var checkbox = document.getElementById('form_checkbox');
+var checkboxchecked = 0; // If checkbox is checked then this variable value is set to 1 or otherwise 0.
+var allinputchecked = 0; // If all inputs are valid then this variable value is set to 1 or otherwise 0.
 
-*/
+checkbox.addEventListener('click', checkterms); // This checking if checkbox is checked or not
+form.addEventListener('submit', validatenull); // This function check every field is null or not if not then submit form.
+firstnamev.addEventListener('blur', validateinput); // This function validate input every time blur event occur.
+lastnamev.addEventListener('blur', validateinput);
+statev.addEventListener('blur', validateinput);
 
+function checkterms() {
+  if (checkbox.checked) {
+    checkboxchecked = 1;
+  } else {
+    checkboxchecked = 0;
+  }
+}
 
+function validatenull(e) {
+  var firstname = document.forms['form']['first_name'].value;
+  var lastname = document.forms['form']['last_name'].value;
+  var state = document.forms['form']['state'].value;
+  var yourmassage = document.forms['form']['your_massage'].value;
 
+  if (
+    firstname == '' ||
+    lastname == '' ||
+    state == '' ||
+    yourmassage == '' ||
+    checkboxchecked == 0 ||
+    allinputchecked == 0
+  ) {
+    e.preventDefault();
 
+    form.classList.add('null_field_msg');
+  } else {
+    form.classList.remove('null_field_msg');
+    form.classList.add('success_field_msg');
+  }
+}
 
+function validateinput(e) {
+  var input = e.target; // Here we save current input element in variable.
+  var regex = /^[a-zA-Z]([a-zA-Z]){0,10}[a-zA-Z]$/; // Regular expression that allow only charecharacters.
+  var str = input.value;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  if (regex.test(str)) {
+    e.preventDefault();
+    console.log('match');
+    input.classList.remove('form_error');
+    input.parentNode.classList.remove('form_error_msg');
+    allinputchecked = 1;
+  } else {
+    console.log('not match');
+    input.classList.add('form_error');
+    input.parentNode.classList.add('form_error_msg');
+    allinputchecked = 0;
+  }
+}
